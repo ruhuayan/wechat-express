8bc4d254b44d69226f783b6f11edf4ffa27eb269
Richard Yan <Ruhua.Yan@mindgeek.com>
2020-01-08 17:06:49 -0500

    add citys to model

--- app/models/address.js
+++ app/models/address.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const AddressSchema = new Schema({
     user: { type: String, ref: 'User', required: '`user`是必填字段' },
     street: { type: String, required: '`street` 是必填字段', maxlength: 255 },
-    city: { type: String, enum: ['Montreal', 'Quebec', 'Toronto'], default: 'Montreal'},
+    city: { type: String, enum: ['Montreal', 'Longueuil', 'Brossard', 'Laval', 'Quebec'], default: 'Montreal'},
     province: { type: String, enum: ['QC', 'ON'], default: 'QC' },
     country: { type: String, enum: ['Canada', '加拿大'], default: 'Canada'},
     postal: { type: String, minlength: 6, maxlength: 6, required: '`postal` 是必填字段'},
